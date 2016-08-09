8b316a2733d6fcdfadc2c1403a7d22e0a2c91b22
Kunal Lillaney <lillaney@jhu.edu>
2016-08-08 21:44:02 -0400

    [test] Fixing path issues in test for now

--- test/test_insertqueue.py
+++ test/test_insertqueue.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import sys
+import os
+sys.path += [os.path.abspath('..')]
 from ndqueue.insertqueue import InsertQueue as IQ
 
 IQ.createQueue()
--- test/test_uploadqueue.py
+++ test/test_uploadqueue.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import sys
+import os
+sys.path += [os.path.abspath('..')]
 from ndqueue.uploadqueue import UploadQueue as UQ
 
 UQ.createQueue()

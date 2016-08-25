fd1d4cf7d8e75edfa0a71eb7c45e79f41d46eeae
Kunal Lillaney <lillaney@jhu.edu>
2016-08-25 11:49:17 -0400

    [worker] Fixing bugs in ndworker.

--- ndworker/ndworker.py
+++ ndworker/ndworker.py
@@ -16,33 +16,34 @@ import ndproj
 from ndqueue.uploadqueue import UploadQueue
 from ndqueue.uploadmessage import UploadMessage
 
-class NdWorker():
+class NDWorker():
 
-  def __init__(self, token, channel, resolution):
+  def __init__(self, token, channel, resolution, file_type='tif'):
     
     # set the channel, res
     self.channel = channel
     self.resolution = resolution
     # load the project here 
     self.proj = ndproj.NDProjectsDB().loadToken(token)
+    self.file_type = file_type
 
 
-  def populateQueue(tile_size=1024, time_interval=0):
+  def populateQueue(self, tile_size=1024, time_interval=1):
     """Populate the message queue"""
     
     # setup the queue
-    queue_name = UploadQueue.createQueue()
+    queue_name = UploadQueue.createQueue([self.proj.getProjectName(), self.channel, str(self.resolution)])
     
     # load the image sizes
     [[ximage_size, yimage_size, zimage_size],(start_time, end_time)] = self.proj.datasetcfg.imageSize(self.resolution)
     # load the image offsets
-    [x_offset, y_offset, z_offset] = proj.datasetcfg.getOffset()[self.resolution]
+    [x_offset, y_offset, z_offset] = self.proj.datasetcfg.getOffset()[self.resolution]
 
     # calculate the number of tiles
     # TODO KL account for xoffset and yoffset here
     num_xtiles = ximage_size / tile_size
     num_ytiles = yimage_size / tile_size
-
+    
     # iterate over time
     for time in range(start_time, end_time, time_interval):
     

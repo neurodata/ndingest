c6e225002ca607f24fb394a50d1c328949a5fa46
Kunal Lillaney <lillaney@jhu.edu>
2016-09-07 00:42:55 -0400

    [lambda] Modifying some stuff in upload lambda

--- ndlambda/functions/upload/uploadlambda.py
+++ ndlambda/functions/upload/uploadlambda.py
@@ -24,26 +24,39 @@ import json
 
 from ndqueue.uploadqueue import UploadQueue
 from ndqueue.insertqueue import InsertQueue
-from nddynamo.ingestdb import IngestDB
+from nddynamo.tileindexdb import TileIndexDB
+
 
 def lambda_handler(event, context):
   """Arrange data in the state database and ready it for ingest"""
- 
+  
+  # setup for testing purposes
+  # IngestDB.createTable()
+  InsertQueue.createQueue()
+  UploadQueue.createQueue()
+
   # extract bucket name and object key from the event
   bucket = event['Records'][0]['s3']['bucket']['name']
-  key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key']).decode('utf8')
+  supercuboid_key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key']).decode('utf8')
+
+  proj_info = extractProjInfo()
   
   # update value in the dynamo table
-  ingest_db = IngestDB()
-  ingest_db.updateItem(key)
+  tileindex_db = TileIndexDB()
+  tileindex_db.putItem(key)
 
   # TODO KL Check if we have all tiles
 
   # insert a new job in the insert queue if we have all the tiles
-  insert_queue = InsertQueue()
-  insert_queue.sendMessage(key)
+  ingest_queue = IngestQueue(proj_info)
+  ingest_queue.sendMessage(supercuboid_key)
 
   # delete message from upload queue
   upload_queue = UploadQueue()
   # KL TODO How do we get the message id in here
-  upload_queue.deleteMessage(message)
+  upload_queue.deleteMessage(message_id, receipt_handle)
+  
+  # cleanup for testing purposes
+  tileindex_db.deleteTable()
+  InsertQueue.deleteQueue()
+  UploadQueue.deleteQueue()

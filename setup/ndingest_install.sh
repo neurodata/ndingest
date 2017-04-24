c42ea36501891cda3c6ef27ccb43dd8d40c5b90d
Alex Eusman <Aeusman@users.noreply.github.com>
2017-04-24 18:26:52 -0400

    Setupchanges (#6)
    [setup] Updates to the install and run service scripts

--- setup/ndingest_install.sh
+++ setup/ndingest_install.sh
@@ -2,10 +2,12 @@
 
 # install fake_s3
 sudo gem install fakes3
+sudo apt-get install default-jre -y
 
 # install fake_sqs
 sudo gem install fake_sqs
 
 # install dynamo
 cd /data/scratch/
-wget http://dynamodb-local.s3-website-us-west-2.amazonaws.com/dynamodb_local_latest.tar.gz .
+wget http://dynamodb-local.s3-website-us-west-2.amazonaws.com/dynamodb_local_latest.tar.gz
+tar -xvzf dynamodb_local_latest.tar.gz
